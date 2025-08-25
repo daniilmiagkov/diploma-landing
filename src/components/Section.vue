71cf0bfe44a0ddcba01ae4fa8bb6d6df3d615ac6
daniilmiagkov <d.miagkov@gmail.com>
2025-08-25 20:20:42 +0300

    move style in section

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -15,5 +15,16 @@
   padding-top: calc(var(--header-height) + var(--section-padding));
   max-width: min(800px, 95%);
   margin: 0 auto var(--space-xl);
+  color: var(--color-secondary);
+  line-height: 1.6;
+  font-size: var(--font-size-lg);
+  margin-bottom: var(--space-2xl);
+  text-align: justify;
+  white-space: normal;
+  overflow-wrap: anywhere;
+  word-wrap: break-word;
+  word-break: keep-all;
+  hyphens: auto;
+  font-weight: 300;
 }
 </style>
--- src/components/sections/Relevance.vue
+++ src/components/sections/Relevance.vue
@@ -145,18 +145,4 @@ onMounted(() => {
   text-align: center;
   line-height: 1.15;
 }
-
-.answer {
-  color: var(--color-secondary);
-  line-height: 1.6;
-  font-size: var(--font-size-lg);
-  margin-bottom: var(--space-2xl);
-  text-align: justify;
-  white-space: normal;
-  overflow-wrap: anywhere;
-  word-wrap: break-word;
-  word-break: keep-all;
-  hyphens: auto;
-  font-weight: light;
-}
 </style>
